4b7e9486faad9824185e5471b2177cbd912bb673
Amazigh-SR <s_Red009@hotmail.com>
2021-03-31 22:39:05 -0400

    Implement dashboard count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,5 +4,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "jungle", password: "book"
   
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
+
 end
